eb3e6cc2ae04611e7375417494fc886fd621539a
Takahiro Matsuzawa <takahiro.matsuzawa.0815@gmail.com>
2026-04-13 16:57:26 +0900

    Add initial Terraform AWS VPC configuration

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
+variable "aws_profile" {
+  type    = string
+  default = "work"
+}
+
+variable "project_name" {
+  type    = string
+  default = "terraform-aws-basic"
+}
