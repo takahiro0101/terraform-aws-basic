59fb575b874b2e31c370d1db9be70610948ecc9f
Takahiro Matsuzawa <takahiro.matsuzawa.0815@gmail.com>
2026-04-14 12:30:00 +0900

    Add public subnet

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "main" {
     Name = "${var.project_name}-vpc"
   }
 }
+
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-1a"
+  }
+}
