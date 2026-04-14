f8e21b56b74ed1450e84a334cd4530a1d4549a66
Takahiro Matsuzawa <takahiro.matsuzawa.0815@gmail.com>
2026-04-14 12:40:28 +0900

    Add route table and internet route

--- main.tf
+++ main.tf
@@ -26,3 +26,22 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.project_name}-igw"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_1a" {
+  subnet_id      = aws_subnet.public_1a.id
+  route_table_id = aws_route_table.public.id
+}
