0bda8098f674c2e30956276aa6be877e0e5e4848
Takahiro Matsuzawa <takahiro.matsuzawa.0815@gmail.com>
2026-04-16 11:52:54 +0900

    Add EC2, security group and comments

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# =========================
+# VPC
+# =========================
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -8,6 +11,9 @@ resource "aws_vpc" "main" {
   }
 }
 
+# =========================
+# Subnet
+# =========================
 resource "aws_subnet" "public_1a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -19,6 +25,9 @@ resource "aws_subnet" "public_1a" {
   }
 }
 
+# =========================
+# Internet Gateway
+# =========================
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
@@ -27,6 +36,9 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+# =========================
+# Route Table
+# =========================
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -44,4 +56,46 @@ resource "aws_route" "default_route" {
 resource "aws_route_table_association" "public_1a" {
   subnet_id      = aws_subnet.public_1a.id
   route_table_id = aws_route_table.public.id
+}
+
+# =========================
+# Security Group
+# =========================
+resource "aws_security_group" "web" {
+  name        = "${var.project_name}-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
+
+# =========================
+# EC2
+# =========================
+resource "aws_instance" "web" {
+  ami           = "ami-0d52744d6551d851e"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public_1a.id
+
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  tags = {
+    Name = "${var.project_name}-ec2"
+  }
 }
